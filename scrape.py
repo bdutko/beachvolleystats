41c3eb6ceedef325e99262005482bddfcfd85ead
Bogdan Dutko <bdutko11@gmail.com>
2025-05-06 22:00:15 -0400

    additional work on tournament scraper and started results scraper

--- scrape.py
+++ scrape.py
@@ -1,6 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-r = requests.get('http://bvbinfo.com/Season.asp?AssocID=3&Year=2025')
-soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.prettify())
+def get_tourneys(year):
+    r = requests.get( f'http://bvbinfo.com/Season.asp?AssocID=3&Year={year}')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    tournaments =[]
+    for result in soup.find_all('a'):
+        link = result.get('href')
+        if 'Tournament' in link:
+            #link = link[link.find('=')+1:]
+            link = "bvbinfo.com/" + link
+            #print(link)
+            tournaments.append(link)
+    return tournaments
+
+
+#print(get_tourneys(2024))
+
+def get_results():
+    tr = requests.get('http://bvbinfo.com/Tournament.asp?ID=4342')
+    tsoup = BeautifulSoup(tr.content, 'html.parser')
+    trimsoup = tsoup.find_all('table')
+    print(trimsoup)
+get_results()
